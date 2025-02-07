ee937489ac49b39ac93e5c14404bec346f8c3c72
jeff0518 <cycivs07@icloud.com>
2025-02-07 22:05:28 +0800

    feat: add css & js

---
+++ challenges/day-007/index.js
@@ -0,0 +1,10 @@
+// 搜尋功能
+document.querySelector(".search-icon").addEventListener("click", function () {
+  document.querySelector(".search-input").classList.toggle("active");
+});
+
+// 選單功能
+document.querySelector(".menu-icon").addEventListener("click", function () {
+  document.querySelector(".panel").classList.toggle("show-menu");
+  document.querySelector(".menu").classList.toggle("active");
+});
